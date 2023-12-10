a6bb5eb6309a7df000d07ec890035d4db87cbfd7
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-12-10 19:33:19 +0900

    Type Alias & index signature

---
+++ dist/chapter4.js
@@ -0,0 +1,27 @@
+//타입 별칭
+let user = {
+    id: 1,
+    name: "이정환",
+    nickname: "winterlood",
+    birth: "1997.01.07",
+    bio: "안녕하세요",
+    location: "부천시",
+};
+/*타입 별칭 사용 시 주의점
+같은 스코프 내에서는 중복으로 변수명을 사용하게 되면 x
+type User = {};
+
+//함수 내부 스코프에서는 ㅇㅋ
+function func() {
+  type User = {};
+}
+*/
+let user2 = {
+    id: 1,
+    name: "hyun",
+    nickname: "purin",
+    birth: "950723",
+    bio: "hi",
+    location: "부천시",
+};
+export {};
